3e30076ec2da7175a86a628f28769901a65bcb85
jpalmat <jimmy.palma1990@gmail.com>
2020-06-07 18:10:58 -0500

    send notification

--- app/src/main/java/com/example/remember/MainActivity.kt
+++ app/src/main/java/com/example/remember/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.remember
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.remember.`interface`.TodoListener
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -18,6 +24,21 @@ class MainActivity : AppCompatActivity(), TodoListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        createNotificationChannel()
+
+        val notification = NotificationCompat.Builder(this, "channelID")
+            .setContentTitle("Bla")
+            .setContentText("ble")
+            .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        val notificationmanager = NotificationManagerCompat.from(this)
+
+        btnNotification.setOnClickListener {
+            notificationmanager.notify(0, notification)
+        }
 
         val firstFragment = FirstFragment()
         val secondFragment = SecondFragment()
@@ -76,6 +97,21 @@ class MainActivity : AppCompatActivity(), TodoListener {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fragment, SecondFragment())
             commit()
+        }
+    }
+
+    /**
+     * notification
+     * https://www.youtube.com/watch?v=urn355_ymNA
+     */
+    fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            val channel = NotificationChannel("channelID", "channelName", NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
         }
     }
 }
